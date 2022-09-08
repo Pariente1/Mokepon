34d8cc4b76db616543583682d77b69715f7c2594
Christian Mendivil <neodante12@gmail.com>
2022-09-08 14:33:47 -0600

    agregamos interaccion con los inputs y alerts

--- JS/PPT.js
+++ JS/PPT.js
@@ -1,3 +1,24 @@
-alert("hola, mundo js")
+function iniciarJuego(){
+    let botonPersonajeJugador = document.getElementById("boton-personaje")
+botonPersonajeJugador.addEventListener('click', seleccionarMascotaPersonaje)
+}
 
-let botonPersonaje 
+function seleccionarMascotaPersonaje(){
+    let inputLeonard = document.getElementById("Leonard")
+    let inputPenny = document.getElementById("Penny")
+    let inputSheldon = document.getElementById("Sheldon")
+    
+    if (inputLeonard.checked){
+        alert("Seleccionaste a Leonard")
+    } else if (inputPenny.checked){
+        alert("Seleccionaste a Penny")
+    } else if (inputSheldon.checked){
+        alert("Seleccionaste a Sheldon")
+    } else {
+        alert("Debes de seleccionar un personaje")
+    }
+}
+
+let botonPersonajeEnemigo 
+
+window.addEventListener('load', iniciarJuego)
